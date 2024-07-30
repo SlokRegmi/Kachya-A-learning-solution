9697bb5ea16a2dcfe22dbee5ad27222924e2fa10
i-nap <Inap.S022@virinchicollege.edu.np>
2024-07-31 00:04:53 +0545

    design finialization

--- kachya/KachyaApp/views.py
+++ kachya/KachyaApp/views.py
@@ -2,7 +2,23 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render (request, "signup_student.html")
+    def teacher_assignment(request):
+        sample_assignments = {
+            "Python": [
+                ["Assignment 1", "Learn the basics of Python programming.", "2024-08-01", "yes", "hello my name is this "]
+
+            ],
+            "Django": [
+                ["Assignment 2",
+                "Build a simple Django application.",
+                "2024-08-15"]
+            ],
+            "JavaScript": [
+                ["Assignment 3",
+                "Create a dynamic web page using JavaScript.",
+                "2024-08-20"]
+            ]}
+    return render (request, "dashboard_teacher.html")
 def about(request):
 
     return render (request, "about.html")  
